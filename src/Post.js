e4a0f9b07b68b26eba9dc1f8614ac1172c6ab850
Soma Sekine <s.sekine.howcollect@gmail.com>
2023-06-02 22:31:51 +0900

    ユーザネームとアバター画像の表示

--- src/Post.js
+++ src/Post.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './Post.css';
+import Avatar from '@mui/material/Avatar'
 
 function Post() {
   return (
     <div className="post">
-        <h3>Username</h3>
-        {/* header -> avatar + username */}
+        <div className="post__header">
+            <Avatar
+                className="post__avatar"
+                alt='Soma'
+                src="/static/images/avatar/1.jpg"
+            />
+            <h3>Username</h3>
+            {/* header -> avatar + username */}
+         </div>
 
         <img className="post__image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Johnrogershousemay2020.webp/1200px-Johnrogershousemay2020.webp" />
         {/* image */}
