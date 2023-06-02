f2f1d897cbd2e41d02b403702eff93c677faa2cc
Soma Sekine <s.sekine.howcollect@gmail.com>
2023-06-02 23:39:30 +0900

    firebaseの設定系

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyAIOQd9NM6Y0yhfTSSrzV9Y45LMcA4j8GM",
+    authDomain: "instagram-clone-react-b13b9.firebaseapp.com",
+    projectId: "instagram-clone-react-b13b9",
+    storageBucket: "instagram-clone-react-b13b9.appspot.com",
+    messagingSenderId: "16137807105",
+    appId: "1:16137807105:web:e038ee213769eff85ea700",
+    measurementId: "G-XDKFS84G2G"
+});
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+
+export { db, auth, storage };
