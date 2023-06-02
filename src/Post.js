3a2fda34ed33b76a1b80b53b161a8a90ce18bfc8
Soma Sekine <s.sekine.howcollect@gmail.com>
2023-06-02 21:35:28 +0900

    投稿のCSSのデザインを調整

--- src/Post.js
+++ src/Post.js
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from 'react';
+import './Post.css';
 
 function Post() {
   return (
-    <div>
+    <div className="post">
         <h3>Username</h3>
         {/* header -> avatar + username */}
 
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Johnrogershousemay2020.webp/1200px-Johnrogershousemay2020.webp" />
+        <img className="post__image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Johnrogershousemay2020.webp/1200px-Johnrogershousemay2020.webp" />
         {/* image */}
 
-        <h4>Username: caption</h4>
+        <h4 className="post__text"><strong>Username </strong>caption</h4>
         {/* username + caption */}
     </div>
   )
