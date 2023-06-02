002876ca6073ca238cca2f26101bbb5316f7b3e8
Soma Sekine <s.sekine.howcollect@gmail.com>
2023-06-02 21:09:05 +0900

    インスタロゴを追加、CSSをリセットと変更

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>hello world</h1>
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt=""
+        />
+      </div>
+
+      {/* Header */}
+      {/* Posts */}
+      {/* Posts */}
     </div>
   );
 }
